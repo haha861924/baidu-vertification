20856527c6fcb842d89916fd6f1cb32551e33628
Elvina <qq861924@gmail.com>
2024-10-13 21:25:43 +0800

    feat: 🎸 add cron job

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 import express from "express";
+import cron from 'node-cron';
+
 import { start as baidu_start } from "./baidu.service.js";
 
 const app = express();
 const port = 3000;
 
 app.listen(port, () => {
-  console.log("Server is running on port 3000");
-  setTimeout(baidu_start, 5000);
+  console.log(`Server is running on port ${port}`);
+
+  // after 5 seconds, start the baidu service
+  // setTimeout(baidu_start, 5000);
 })
+
+// cron job to run the baidu service every 10 seconds
+cron.schedule("*/10 * * * * *", () => {
+  console.log('running a task every 10 seconds');
+  // baidu_start();
+});
+
