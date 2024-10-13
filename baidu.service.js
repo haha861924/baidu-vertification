7594d852422d8e461a21f95e85b42d860ce23e33
Elvina <qq861924@gmail.com>
2024-10-13 21:18:54 +0800

    feat: 🎸 crawler baidu.com article title

--- baidu.service.js
+++ baidu.service.js
@@ -1,3 +1,24 @@
+import puppeteer from 'puppeteer';
+
+const crawler_path = 'https://tieba.baidu.com/f?kw=nba'
+
+
 export async function start() {
   console.log('Starting Puppeteer');
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    devtools: true
+  });
+
+  const page = await browser.newPage();
+  await page.goto(crawler_path);
+
+  // do something with the page
+  // e.g. get the title of the page
+  const title = await page.title();
+  console.log('Title:', title);
+
+  // close the browser
+  await browser.close();
 }
